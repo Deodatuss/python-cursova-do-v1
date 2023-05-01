377a85690af6805b7b4c83bab1d7729a5e0e276e
Deodatuss <serbgan03@gmail.com>
2023-05-01 16:30:02 +0300

    change names and clean bruteforce file

--- src/bruteforce.py
+++ src/bruteforce.py
@@ -4,19 +4,25 @@ import utilities
 import itertools
 import numpy as np
 
-folder = "src/"
+folder = "data/"
 input_relative_filename = folder + "input.json"
-output_relative_filename = folder + "output.json"
-how_much_to_choose = [1, 2, 3]
+output_relative_filename = folder + "bruteforce_output.json"
+how_much_to_choose = [2, 3, 1]
+
+# generates all possible permutations for given how_much_to_choose
+#  i.e. even if given [2, 3, 1],  it will anyway also generate and use [3, 2, 1], [2, 1, 3] etc
+
 elements_to_choose_from_each_group = list(
     itertools.permutations(how_much_to_choose))
+elements_to_choose_from_each_group = [how_much_to_choose]
 
 data = converters.JSONToNumpy(input_relative_filename)
 
 groups_info = utilities.GetGroupsSize(data)
 
-a = [groups_info["group_A"]["size"], groups_info["group_B"]
-     ["size"], groups_info["group_C"]["size"]]
+a = [groups_info["a"]["size"], groups_info["b"]
+     ["size"], groups_info["c"]["size"]]
+
 
 # indices of elements from each group
 # e.g. [0, 1], [2, 3], [4, 5] if array of 6 elements was divided in 3 groups by 2
@@ -32,9 +38,7 @@ for i in elements_to_choose_from_each_group:
     all_combinations *= len(list(itertools.combinations(group_B, i[1])))
     all_combinations *= len(list(itertools.combinations(group_C, i[2])))
 
-    # print("for ", i, ": ", all_combinations)
     summed += all_combinations
-# print(summed)
 
 
 # gather all possible combinations
@@ -59,7 +63,7 @@ for i in elements_to_choose_from_each_group:
 
 
 # count corresponded row-by-row effectivenes value for all possible combinations
-corr_effectiveness = np.zeros((summed, 1), np.float16)
+corr_effectiveness = np.zeros((summed, 1), np.float32)
 data_indeces = [x for x in range(a[0]+a[1]+a[2])]
 
 counter = 0
@@ -70,12 +74,17 @@ for row in pos_combinations:
     for index in row:
         sum += np.sum(data[index][mask])
 
+    sum = (sum - 14)/2
+
     corr_effectiveness[counter] = sum
     counter += 1
 
-for i in corr_effectiveness:
-    print("{:.4f}".format(i[0]))
-
 unique, counts = np.unique(corr_effectiveness, return_counts=True)
 
-print(dict(zip(unique, counts)))
+unique_counts = {}
+for item in zip(unique, counts):
+    unique_counts.update({str(item[0]): int(item[1])})
+
+print(unique_counts)
+
+converters.DictToJSONFile(unique_counts, output_relative_filename)
