19144c8e5e4d143d4eba49581f0fd5852f7a6060
Deodatuss <serbgan03@gmail.com>
2023-05-10 15:14:19 +0300

    change demo examples positions

--- src/BnBDemo.py
+++ src/BnBDemo.py
@@ -2,23 +2,23 @@ import branchAndBound as bnb
 import converters
 import utilities
 from contextlib import redirect_stdout
+import json
 
 
 def main():
     """
-    This function is used as a test and presentation for file functions
+    This function is used as a test and presentation for BnB functions and workflow
     """
-    folder = "data/"
+    folder = "data/demo/"
     input_relative_filename = folder + "input.json"
-    output_relative_filename = folder + "output.json"
+    dict_output_relative_filename = folder + "dict_output.json"
+    tree_output_relative_filename = folder + "tree_output.txt"
 
-    test_indices = {
-        'a': [0, 1, 2, 3, 4, 5], 'b': [6, 7, 8, 9, 10], 'c': [11, 12, 13, 14]
-    }
-
+    # number of elements from each of three groups
     how_much_to_choose = {
         'a': 1, 'b': 3, 'c': 2
     }
+
     data = converters.JSONToNumpy(input_relative_filename)
     groups_info = utilities.GetGroupsSize(data)
 
@@ -37,22 +37,37 @@ def main():
         "c": group_C
     }
 
-    HB = bnb.HighBound(data, group_indices, how_much_to_choose)
+    # group_indices looks like this:
+    # group_indices = {
+    #     'a': [0, 1, 2, 3, 4, 5], 'b': [6, 7, 8, 9, 10], 'c': [11, 12, 13, 14]
+    # }
+
+    high_bound = bnb.HighBound(data, group_indices, how_much_to_choose)
 
     tr_tree = bnb.BranchAndBound(
         data, group_indices, how_much_to_choose, max_samples_for_branch=1, start_level=0)
 
-    tree_dict = {
-        -1: ["A1; 10,2", "A2; 9,8", "A3; 10,0"],
-        "A1; 10,2": ["A1-B1; 8,2", "A1-B4; 8,2"],
-        "A3; 10,0": ["A3-B1; 8,4", "A3-B3; 8,7"],
-        "A2; 9,8": ["X"],
-        "A1-B4; 8,2": ["A1-B4-C1; 7,2", "A1-B4-C2; 7,5", "A1-B4-C3; 7,3"]
-    }
+    dict_for_json = {}
+
+    for level in tr_tree:
+        dict_for_json.update(level)
+
+    # # save tr_tree dict to json file
+    with open(dict_output_relative_filename, 'w') as fp:
+        json.dump(dict_for_json, fp)
 
     final_tree, max_dict = bnb.DataDictToTreedictConverter(tr_tree)
 
-    with open('tree_output.txt', 'w', encoding="utf-8") as f:
+    # final tree has a structure similar to this:
+    # test_tree_dict = {
+    #     -1: ["A1; 10,2", "A2; 9,8", "A3; 10,0"],
+    #     "A1; 10,2": ["A1-B1; 8,2", "A1-B4; 8,2"],
+    #     "A3; 10,0": ["A3-B1; 8,4", "A3-B3; 8,7"],
+    #     "A2; 9,8": ["X"],
+    #     "A1-B4; 8,2": ["A1-B4-C1; 7,2", "A1-B4-C2; 7,5", "A1-B4-C3; 7,3"]
+    # }
+
+    with open(tree_output_relative_filename, 'w', encoding="utf-8") as f:
         with redirect_stdout(f):
             utilities.ptree(-1, final_tree, indent_width=9)
 
